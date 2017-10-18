10e8fc2bbc005f6453f11ab1db30941d84d355cb
Marc Torruella <rollwhistler@gmail.com>
2017-10-18 18:31:29 +0200

    004b-lifecycle-hooks

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component, ViewChild } from '@angular/core';
-import {ChildviewExampleComponent} from './childview-example/childview-example.component';
+import { Component, ViewChild, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { ChildviewExampleComponent } from './childview-example/childview-example.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, AfterViewChecked {
   title = 'app';
   numCopies = 3;
   currentHeight = 0;
+  initial = "Ola K Ase";
   @ViewChild(ChildviewExampleComponent) childRef: ChildviewExampleComponent;
 
   newHeight() {
     this.currentHeight = this.childRef.currentHeight;
+  }
+
+  ngAfterViewInit() {
+    console.log("View Init");
+  }
+
+  ngAfterViewChecked() {
+    console.log("View Checked");
+  }
+
+  alterInput() {
+    this.initial += " < ! > ";
   }
 }
--- client/src/app/childview-example/childview-example.component.ts
+++ client/src/app/childview-example/childview-example.component.ts
@@ -1,19 +1,49 @@
-import { Component, OnInit, Input, ElementRef, Output, EventEmitter } from '@angular/core';
+import {
+  Component, OnInit, Input, ElementRef, Output, EventEmitter,
+  OnChanges, DoCheck, AfterContentInit, AfterContentChecked, OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-childview-example',
   templateUrl: './childview-example.component.html',
   styleUrls: ['./childview-example.component.css']
 })
-export class ChildviewExampleComponent implements OnInit {
+export class ChildviewExampleComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, OnDestroy {
   @Input() initial: string;
   @Output() onChange: EventEmitter<boolean> = new EventEmitter<boolean>();
   public currentHeight: number;
 
   constructor(public textRef: ElementRef) { }
 
+  //Respond when Angular (re)sets data-bound input properties. The method receives a SimpleChanges object of current and previous property values.
+  ngOnChanges(e) {
+    console.log("On Changes: ");
+    console.log(JSON.stringify(e));
+  }
+
+  //Initialize the directive/component after Angular first displays the data-bound properties and sets the directive/component's input properties.
   ngOnInit() {
     this.currentHeight = 0;
+    console.log("On Init");
+  }
+
+  //Detect and act upon changes that Angular can't or won't detect on its own.
+  ngDoCheck() {
+    console.log("Do Check");
+  }
+
+  //Respond after Angular projects external content into the component's view.
+  ngAfterContentInit() {
+    console.log("After Content Init");
+  }
+
+  //Respond after Angular checks the content projected into the component.
+  ngAfterContentChecked() {
+    console.log("After Content Checked");
+  }
+  //Cleanup just before Angular destroys the directive/component. Unsubscribe Observables and detach event handlers to avoid memory leaks
+  ngOnDestroy() {
+    console.log("On Destroy");
   }
 
   keyUp(event) {
