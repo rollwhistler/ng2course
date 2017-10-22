24a5eb506cc774437f07e4d1ca4fc183d701ea97
Marc Torruella <rollwhistler@gmail.com>
2017-10-22 10:06:28 +0200

    011-reactive-forms-pre-validation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
 
@@ -11,7 +11,7 @@ import { UserFormComponent } from './components/user-form/user-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/user-form/user-form.component.ts
+++ client/src/app/components/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { User, UserRole, UserRoleFactory } from '../../models/user';
 
 @Component({
@@ -9,17 +10,30 @@ import { User, UserRole, UserRoleFactory } from '../../models/user';
 export class UserFormComponent implements OnInit {
 
   public roles: UserRole[] = new UserRoleFactory().getDefaultRoles();
-  public model: UserRole;
+  public userForm: FormGroup; /*= new FormGroup({
+    name: new FormControl(),
+    lastname: new FormControl(),
+    roleId: new FormControl()
 
-  constructor() {
-    this.model = new User();
+  });*/
+
+  constructor(public fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      lastname: ['', Validators.required],
+      roleId: ['', Validators.required]
+    })
   }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    console.log(this.model);
+    console.log(this.userForm);
   }
 
 }
