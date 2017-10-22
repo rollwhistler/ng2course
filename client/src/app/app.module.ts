ceaa271de5bbbefae321b1383c453fb7e5c0185b
Marc Torruella <rollwhistler@gmail.com>
2017-10-22 10:19:06 +0200

    010-template-driven-custom validator

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
+import { ForbiddenValidatorDirective } from './utils/forms/validators/forbidden-validator.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserFormComponent
+    UserFormComponent,
+    ForbiddenValidatorDirective
   ],
   imports: [
     BrowserModule,
