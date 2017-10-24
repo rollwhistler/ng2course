4fe2b92de05c6b0a8ac9ebf1e8f365ac59341e46
Marc Torruella <rollwhistler@gmail.com>
2017-10-24 21:39:27 +0200

    initial commit

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -7,7 +7,9 @@ import { LogginService } from '../../services/loggin.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public bitches: number = 2;
+  public gender: string = 'm';
+  public users: any[] = new Array(4);
   constructor(public ls: LogginService) { }
 
   ngOnInit() {
