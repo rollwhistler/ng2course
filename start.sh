3572dfcd1cf0097656d494ee6906eb551fe218dd
Marc Torruella <rollwhistler@gmail.com>
2017-10-24 22:53:37 +0200

    failed experiment

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -it -d --name ng2course -p 4200:4200 --mount type=bind,source="$(pwd)/app",target=/app ng2course
+docker run -it -d --name ng2course -p 4200:4200 --mount type=bind,source="$(pwd)/client/dist",target=/app ng2course
